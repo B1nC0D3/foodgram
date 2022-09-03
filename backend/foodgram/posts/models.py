b8b9c94bb52e5ad5e7cb7b3ff913553dc064d7bf
Kirill Kudryashov <b1nanon@mail.ru>
2022-09-03 21:44:16 +0300

    Сделал модели

--- backend/foodgram/posts/models.py
+++ backend/foodgram/posts/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tags(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=50,
+        )
+    color = models.CharField(
+        unique=True,
+        max_length=6,
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+
+
+class Products(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    measure = models.CharField(
+        max_length=10,
+    )
+
+
+class Ingredients(models.Model):
+    ingredient = models.ForeignKey(
+        Products,
+        on_delete=models.CASCADE,
+    )
+    amount = models.SmallIntegerField()
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+    name = models.CharField(
+        max_length=50,
+    )
+    pic = models.ImageField()
+    description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredients)
+    tag = models.ManyToManyField(Tags)
+    cooking_time = models.IntegerField()
