f80cee168e7a3cd1046dcdbe15716c783fa02036
Kirill Kudryashov <b1nanon@mail.ru>
2022-09-11 15:56:57 +0300

    Доработал систему фильтрации

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,9 +1,18 @@
-from posts.models import Recipes
+from posts.models import Recipes, Tags
 import django_filters
 
 
+def get_choices():
+    tags = Tags.objects.all()
+    result = []
+    for tag in tags:
+        result.append((tag.name, tag.name))
+    print(result)
+    return result
+
 class RecipeFilter(django_filters.FilterSet):
-    tags = django_filters.CharFilter(field_name='tags__name', lookup_expr='iexact')
+    tags = django_filters.MultipleChoiceFilter(field_name='tags__name', choices=get_choices())
     class Meta:
         model = Recipes
-        fields = ('tags',)
+        fields = ('tags', 'author')
+    
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -117,18 +117,18 @@ CORS_URLS_REGEX = r'^/api/.*$'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-    #     'NAME': os.getenv('DB_NAME', 'postgres'),
-    #     'USER': os.getenv('POSTGRES_USER', 'postgres'),
-    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-    #     'HOST': os.getenv('DB_HOST', 'db'),
-    #     'PORT': os.getenv('DB_PORT', 5432)
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': 'mydatabase',
+    # }
 }
 
 
